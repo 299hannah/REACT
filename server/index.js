0927094e11485128e84eb9bb462c95d17b4811db
299hannah <hannahmwangiwambui@gmail.com>
2022-08-19 09:44:44 +0300

    creating socket io event listener on the server"

--- server/index.js
+++ server/index.js
@@ -1,14 +1,26 @@
 const express = require('express');
 const app = express();
-const http = require ('http');
+http = require ('http');
 const cors = require('cors');
+const { Server } = require('socket.io');
 
 app.use(cors());
 
 const server = http.createServer(app);
 
-app.get('/', (req, res) => {
-    res.send('hi')
+const io = new Server (server, {
+    cors: {
+        origin: 'http://localhost:3000',
+        methods: ['GET', 'POST'],
+    },
 });
+
+io.on('connection', (socket)=>{
+    console.log(`user connected ${socket.id}`);
+});
+
+// app.get('/', (req, res) => {
+//     res.send('hi')
+// });
 
 server.listen(4000, () => 'server is running on port 4000');
