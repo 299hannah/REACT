15d3a05c6cd2462df89e466c35e993a4b7e8e8ce
299hannah <hannahmwangiwambui@gmail.com>
2022-08-19 10:53:41 +0300

    send message to users room

--- server/index.js
+++ server/index.js
@@ -1,8 +1,12 @@
+const CHAT_BOT = 'ChatBot';
+let chatRoom = '';
+let allUsers = [];
 const express = require('express');
 const app = express();
 http = require ('http');
 const cors = require('cors');
 const { Server } = require('socket.io');
+const { Socket } = require('dgram');
 
 app.use(cors());
 
@@ -15,14 +19,32 @@ const io = new Server (server, {
     },
 });
 
+// listens when client connects via socket.oi-client 
 io.on('connection', (socket)=>{
     console.log(`user connected ${socket.id}`);
 
-
+// add user room
     socket.on('join_room',(data) =>{
         const { username, room } = data;
         socket.join(room);
-});
+
+        let __createdtime__ = Date.now();
+        socket.to(room).emit('receive_message',{
+            message: `${username} just joined the room`,
+            username: CHAT_BOT,
+            __createdtime__,
+        });
+        Socket.emit('receive_message', {
+            message: `Welcome ${username}`,
+            username: CHAT_BOT,
+            __createdtime__,
+        });
+        chatRoom =room;
+        allUsers.push({id:socket.id, room});
+        chatRoomUsers = allUsers.filter((user)=> user.room === room);
+        socket.to(room).emit('chatroom_users', chatRoomUsers);
+        socket.emit('chatroom_users', chatRoomUsers);
+    });
 });
 // app.get('/', (req, res) => {
 //     res.send('hi')
